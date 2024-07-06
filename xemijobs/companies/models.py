f33a584cdc11722a604cf3859e67cb8fd0cdbe03
Victor GC <vgarcanuk@gmail.com>
2024-07-06 22:23:45 +0100

    CHORE: finalize Models implementations from TODO comments for Users and Companies

--- xemijobs/companies/models.py
+++ xemijobs/companies/models.py
@@ -41,6 +41,30 @@ class Company(UserMixin):
             )
         return None
         
+ 
+    def get_id(self):
+        return self.id
+
+# TODO:
+# Include the CRUD-Funtions in the model
+
+#! create the {CRUD-functions}
+
+    ## Create
+    #! no Login needed
+    ##? create_new_userComp function
+    @staticmethod
+    def create_new_user(username, password, role):
+        # Create a new user document in the database
+        new_user_data = {
+            'username': username,
+            'password': password,
+            'role': role
+        }
+        mongo.db.users.insert_one(new_user_data)
+    ## Read
+    #! no Login needed
+    ##? get_by_id function - fetch one userComp by the id
     @staticmethod
     def get_by_id(user_id):
         """
@@ -71,25 +95,59 @@ class Company(UserMixin):
             # If an error occurs, print the error message and return None
             print("==============> Exception in get_by_id:", e)
             return None
-    
-    def get_id(self):
-        return self.id
+   
 
-# TODO:
-# Include the CRUD-Funtions in the model
-
-#! create the {CRUD-functions}
-
-    ## Create
-    #! no Login needed
-    ##? create_new_userComp function
-    ## Read
-    #! no Login needed
-    ##? get_by_id function - fetch one userComp by the id
     ##? get_by_username function - fetch one userComp by username
+    @staticmethod
+    def get(username):
+        """
+        Retrieve a Company object by its username.
+
+        Parameters:
+        username (str): The unique username of the company.
+
+        Returns:
+        Company: A Company object if found, otherwise None.
+
+        Raises:
+        None
+
+        Example:
+        company = Company.get('john_doe')
+        if company:
+            print(f"Company found: {company.username}")
+        else:
+            print("Company not found.")
+        """
+        user_data = mongo.db.companies.find_one({'username': username})
+        if user_data:
+            return Company(
+                username=user_data['username'], 
+                password=user_data['password'],
+                _id=str(user_data['_id']),
+                role=user_data['role']
+            )
+        return None
+    
+
     ## Update
     #! only COMPANIES
     ##? update_profile function
+    @staticmethod
+    def update_profile(**profile_data):
+        # Retrieve the current user's ID
+        current_user_id = str(login_manager.current_user.get_id())
+        # Update the user's profile in the database
+        mongo.db.companies.update_one(
+            {'_id': ObjectId(current_user_id)},
+            {"$set": profile_data})
+    
     ## Delete
     #! only COMPANIES
     ##? delete_user function
+    @staticmethod
+    def delete_user():
+        # Retrieve the current user's ID
+        current_user_id = str(login_manager.current_user.get_id())
+        # Delete the user from the database
+        mongo.db.companies.delete_one({'_id': ObjectId(current_user_id)})
--- xemijobs/users/models.py
+++ xemijobs/users/models.py
@@ -10,31 +10,32 @@ class User(UserMixin):
         self.id = str(_id) 
         self.role = role
     
-    @staticmethod
-    def get(username):
-        """
-        Retrieve a user from the database by their username.
-
-        Parameters:
-        username (str): The username of the user to retrieve.
-
-        Returns:
-        User: An instance of the User class representing the retrieved user.
-            If no user is found, returns None.
-
-        Raises:
-        None
-        """
-        user_data = mongo.db.users.find_one({'username': username})
-        if user_data:
-            return User(
-                username=user_data['username'], 
-                password=user_data['password'],
-                _id=str(user_data['_id']),
-                role=user_data['role']
-            )
-        return None
     
+    def get_id(self):
+        return self.id
+
+# TODO:
+# Include the CRUD-Funtions in the model
+
+#! create the {CRUD-functions}
+
+    ## Create
+    #! no Login needed
+    ##? create_new_user function
+    @staticmethod
+    def create_new_user(username, password, role):
+        # Create a new user document in the database
+        new_user_data = {
+            'username': username,
+            'password': password,
+            'role': role
+        }
+        mongo.db.users.insert_one(new_user_data)
+
+
+    ## Read
+    #! no Login needed
+    ##? get_by_id function - fetch one user by the id
     @staticmethod
     def get_by_id(user_id):
         """
@@ -65,25 +66,54 @@ class User(UserMixin):
             # If an error occurs, print the error message and return None
             print("==============> Exception in get_by_id:", e)
             return None
-    
-    def get_id(self):
-        return self.id
-
-# TODO:
-# Include the CRUD-Funtions in the model
-
-#! create the {CRUD-functions}
-
-    ## Create
-    #! no Login needed
-    ##? create_new_user function
-    ## Read
-    #! no Login needed
-    ##? get_by_id function - fetch one user by the id
+   
+   
     ##? get_by_username function - fetch one user by username
+    @staticmethod
+    def get(username):
+        """
+        Retrieve a user from the database by their username.
+
+        Parameters:
+        username (str): The username of the user to retrieve.
+
+        Returns:
+        User: An instance of the User class representing the retrieved user.
+            If no user is found, returns None.
+
+        Raises:
+        None
+        """
+        user_data = mongo.db.users.find_one({'username': username})
+        if user_data:
+            return User(
+                username=user_data['username'], 
+                password=user_data['password'],
+                _id=str(user_data['_id']),
+                role=user_data['role']
+            )
+        return None
+ 
+
     ## Update
     #! only USERS
     ##? update_profile function
+    @staticmethod
+    def update_profile(**profile_data):
+        # Retrieve the current user's ID
+        current_user_id = str(login_manager.current_user.get_id())
+        # Update the user's profile in the database
+        mongo.db.users.update_one(
+            {'_id': ObjectId(current_user_id)},
+            {"$set": profile_data})
+        
+
     ## Delete
     #! only USERS
     ##? delete_user function
+    @staticmethod
+    def delete_user():
+        # Retrieve the current user's ID
+        current_user_id = str(login_manager.current_user.get_id())
+        # Delete the user from the database
+        mongo.db.users.delete_one({'_id': ObjectId(current_user_id)})
