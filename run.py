228b870b766b6e611437838b9c2dd00c3ac57236
Victor GC <vgarcanuk@gmail.com>
2024-07-13 01:54:59 +0100

    FIX: adjustments for Heroku deployment

--- run.py
+++ run.py
@@ -6,4 +6,7 @@ load_dotenv()
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=os.getenv('DEBUG'))
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
