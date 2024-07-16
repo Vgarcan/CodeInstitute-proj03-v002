82d59c761761f8a8593c4746f2ea00232366c121
Victor GC <vgarcanuk@gmail.com>
2024-07-16 21:55:55 +0100

    FIX: Deleted exposed URI

--- config.py
+++ config.py
@@ -6,6 +6,5 @@ load_dotenv()
 
 class Config:
     
-    # SECRET_KEY = 'mongodb+srv://mongo_xema:Iv7m9uQdjlg3yoTn@jsearchdb.m9rzgkr.mongodb.net/jsearchweb'
     SECRET_KEY = os.getenv('SECRET_KEY')
     MONGO_URI = os.getenv('MONGO_URI')
